3cf0f7cc1127151ddd845867e9c3c20500d2bbe3
dmalicia <dmalicia@gmail.com>
2020-09-18 21:51:52 -0400

    add count 2

--- frontend/main.tf
+++ frontend/main.tf
@@ -22,7 +22,7 @@ resource "google_compute_address" "static" {
 
 // A single Compute Engine instance
 resource "google_compute_instance" "frontendn" {
- count        = "${var.node_count}"
+ count        = var.node_count
  name         = "dm-frontend-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
@@ -44,10 +44,6 @@ metadata_startup_script = file("/puppet.sh")
      nat_ip = google_compute_address.static.address
    }
  }
-}
-
-output "controller" {
-  value = google_compute_instance.frontend.0.network_interface.0.access_config.0.nat_ip
 }
 
 resource "google_compute_firewall" "frontendn" {
