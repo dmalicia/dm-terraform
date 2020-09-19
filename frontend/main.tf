9ba2ba64a46b5748719908820f2bdbcf866a8d13
dmalicia <dmalicia@gmail.com>
2020-09-18 21:56:53 -0400

    add count 2

--- frontend/main.tf
+++ frontend/main.tf
@@ -23,7 +23,7 @@ resource "google_compute_address" "static" {
 // A single Compute Engine instance
 resource "google_compute_instance" "frontendn" {
  count        = var.node_count
- name         = "dm-frontend-${random_id.instance_id.hex}"
+ name         = "dm-frontend-${count.index}${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
 
