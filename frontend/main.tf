82003f9f283b948f43a7510c934869f8c964e9bc
dmalicia <dmalicia@gmail.com>
2020-09-18 21:14:51 -0400

    chaning name to recreate

--- frontend/main.tf
+++ frontend/main.tf
@@ -18,7 +18,7 @@ resource "google_compute_address" "static" {
 
 // A single Compute Engine instance
 resource "google_compute_instance" "frontend" {
- name         = "dm-frontend-${random_id.instance_id.hex}"
+ name         = "dm-frontend2-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
 
