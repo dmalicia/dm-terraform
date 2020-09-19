34437361fc2e6c673cd589f6507640be31560be9
dmalicia <dmalicia@gmail.com>
2020-09-18 21:50:39 -0400

    add count 2

--- frontend/main.tf
+++ frontend/main.tf
@@ -11,14 +11,18 @@ resource "random_id" "instance_id" {
  byte_length = 8
 }
 
+variable "node_count" {
+  default = "2"
+ }
+
 // Resource for ips
 resource "google_compute_address" "static" {
   name = "dm-frontend-${random_id.instance_id.hex}"
 }
 
 // A single Compute Engine instance
-resource "google_compute_instance" "frontend-${random_id.instance_id.hex}" {
- count        = "2" 
+resource "google_compute_instance" "frontendn" {
+ count        = "${var.node_count}"
  name         = "dm-frontend-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
@@ -46,7 +50,7 @@ output "controller" {
   value = google_compute_instance.frontend.0.network_interface.0.access_config.0.nat_ip
 }
 
-resource "google_compute_firewall" "frontend" {
+resource "google_compute_firewall" "frontendn" {
  name    = "frontend-firewall"
  network = "default"
 
