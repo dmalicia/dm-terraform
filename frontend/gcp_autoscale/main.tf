c1d46ef22c1051f2244d9f601014bbd2799c01b9
dmalicia <dmalicia@gmail.com>
2020-09-19 23:59:14 -0400

    dm-autoscale-pre

--- frontend/gcp_autoscale/main.tf
+++ frontend/gcp_autoscale/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" { 
     bucket    = "dmustf"
-    prefix    = "/dmus/frontend-gcp-nodes-tf"
+    prefix    = "/dmus/frontend-gcp-asg-tf"
     credentials = "/creds.json"
   }
 }
@@ -15,34 +15,91 @@ resource "random_id" "instance_id" {
  byte_length = 8
 }
 
-resource "google_compute_address" "static" {
-  count = "${var.nodes[terraform.workspace]}"
-  name = "dmlc-frontend-${count.index}${random_id.instance_id.hex}"
-  region = var.regions[terraform.workspace]
+resource "google_compute_address" "asg" {
+  name = "dmlc-autoscale-${random_id.instance_id.hex}"
 }
 
-resource "google_compute_instance" "frontend" {
- count = "${var.nodes[terraform.workspace]}"
- name         = "dmlc-frontend-${terraform.workspace}-${count.index}"
- machine_type = "f1-micro"
- zone         = var.zones[terraform.workspace] 
-
- boot_disk {
-   initialize_params {
-     image = "debian-cloud/debian-9"
-   }
- }
-
-metadata_startup_script = file("${var.scbootstrap}/puppet.sh")
- metadata = {
-   ssh-keys = "dmalicia:${file("${var.scpath}/id_rsa.pub")}"
-            }
-
- network_interface {
-   network = "default"
-
-   access_config {
-     nat_ip = google_compute_address.static[count.index].address
-   }
- }
+# Create a Google Compute Forwarding Rule
+resource "google_compute_forwarding_rule" "asg" {
+  name       = "asg-forwarding-rule"
+  target     = "${google_compute_target_pool.asg.self_link}"
+  port_range = "80"
+  ip_address = "${google_compute_address.asg.address}"
 }
+
+resource "google_compute_target_pool" "asg" {
+  name          = "asg-target-pool"
+  health_checks = ["${google_compute_http_health_check.asg.name}"]
+}
+
+# Create a Google Compute Http Health Check
+resource "google_compute_http_health_check" "asg" {
+  name                 = "asg-health-check"
+  request_path         = "/"
+  check_interval_sec   = 30
+  timeout_sec          = 3
+  healthy_threshold    = 2
+  unhealthy_threshold  = 2
+  port                 = "${var.server_port}"
+}
+
+#---------------------------------------------------------------------
+
+# Create a Google Compute instance Group Manager
+resource "google_compute_instance_group_manager" "asg" {
+  name = "asg-group-manager"
+  zone = "us-east1-b"
+  version { 
+  instance_template  = "${google_compute_instance_template.asg.self_link}"
+  }
+  target_pools       = ["${google_compute_target_pool.asg.self_link}"]
+  base_instance_name = "asg"
+}
+
+resource "google_compute_autoscaler" "asg" {
+  name   = "my-autoscaler"
+  zone   = "us-central1-f"
+  target = google_compute_instance_group_manager.asg.id
+
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 1
+    cooldown_period = 60
+
+    metric {
+      name                       = "pubsub.googleapis.com/subscription/num_undelivered_messages"
+    }
+  }
+}
+
+# Create a Google Compute instance Template
+resource "google_compute_instance_template" "asg" {
+  machine_type  = "f1-micro"
+
+  disk {
+    source_image = "ubuntu-1604-lts"
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata_startup_script = "echo 'Hello, World' > index.html ; nohup busybox httpd -f -p ${var.server_port} &"
+}
+
+#---------------------------------------------------------------------
+/*
+# Create a Google Compute Backend Service
+resource "google_compute_backend_service" "asg" {
+  name        = "asg-backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+  enable_cdn  = false
+  backend {
+    group = "${google_compute_instance_group_manager.asg.instance_group}"
+  }
+  health_checks = ["${google_compute_http_health_check.asg.self_link}"]
+}
+*/
+#---------------------------------------------------------------------
--- frontend/gcp_autoscale/variables.tf
+++ frontend/gcp_autoscale/variables.tf
@@ -59,3 +59,8 @@ variable "nodes" {
     asia = "1"
   }
 }
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = "80"
+}
