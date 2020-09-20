98cbda6a2dc32b5ba3e1ffba01d3cde5fe0e66f3
dmalicia <dmalicia@gmail.com>
2020-09-19 21:55:34 -0400

    test

--- frontend/gcp_nodes/main.tf
+++ frontend/gcp_nodes/main.tf
@@ -27,7 +27,7 @@ resource "google_compute_address" "static" {
 
 // A single Compute Engine instance
 resource "google_compute_instance" "frontend" {
- count        = var.node_count
+ count = "${var.nodes[terraform.workspace]}"
  name         = "dmlc-frontend-${count.index}${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
