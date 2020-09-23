def2999169324fbee8e70bd6ce421eccab58ba71
dmalicia <dmalicia@gmail.com>
2020-09-23 01:23:51 -0400

    backend configs

---
+++ backend/gcp_autoscale/terraform.tfvars
@@ -0,0 +1,10 @@
+# GCP Settings
+gcp_project = "2036174436"
+gcp_region = "us-central1"
+gcp_auth_file = "/creds.json"
+# Bucket
+bucket-name   = "dmustf" 
+storage-class = "REGIONAL"
+# DNS Settings
+dns_name = "dmlc"
+dns_domain = "dmlc.pw."
---
+++ backend/gcp_autoscale/variables.tf
@@ -0,0 +1,79 @@
+# define GCP region
+variable "gcp_region" {
+  type        = string
+  description = "GCP region"
+}
+# define GCP project name
+variable "gcp_project" {
+  type        = string
+  description = "GCP project name"
+}
+# GCP authentication file
+variable "gcp_auth_file" {
+  type        = string
+  description = "GCP authentication file"
+}
+variable "bucket-name" {
+  default     = "dmustf"
+}
+variable "storage-class" {
+  type        = string
+  description = "The storage class of the Storage Bucket to create"
+}
+
+# Source code folder
+variable "scbootstrap" {
+  type       = string
+  default    = "/usr/local/share/dm-terraform/backend/bootstrap/"
+}
+
+# Seed Creds
+variable "scpath" {
+  type       = string
+  default    = "/usr/local/share/dm-terraform/seed/bootstrap/"
+}
+
+variable "zones" {
+  type      = map
+  default = {
+    amer = [ "us-west4-b", "us-east4-b" ] 
+    euro = [ "europe-west6-a", "europe-north1-a" ]
+    ocea = [ "australia-southeast1-b" ]
+    asia = [ "asia-east2-b", "asia-south1-b" ] 
+}
+}
+
+variable "regions" {
+  type      = map
+  default = {
+    amer = [ "us-west4", "us-east4" ]
+    euro = [ "europe-west6", "europe-north1" ]
+    ocea = [ "australia-southeast1" ] 
+    asia = [ "asia-east2" , "asia-south1" ]
+}
+}
+
+variable "asg_per_region" {
+  type      = map
+  default = { 
+    amer = "1"
+    euro = "1"
+    asia = "0"
+    ocea = "0"
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = "80"
+}
+
+variable "dns_name" {
+  type        = string
+  description = "The name of the DNS"
+}
+variable "dns_domain" {
+  type        = string
+  description = "The domain of the DNS"
+}
+
