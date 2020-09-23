6861ec5f3212ff4dc69778f328c4e5d25121614f
Diego Malicia <dmalicia@gmail.com>
2020-09-23 04:54:46 -0400

    Revert "fixing the asg size"

--- backend/gcp_autoscale/main.tf
+++ backend/gcp_autoscale/main.tf
@@ -81,7 +81,7 @@ resource "google_compute_autoscaler" "asg" {
 
   autoscaling_policy {
     max_replicas    = 2
-    min_replicas    = 2
+    min_replicas    = 1
     cooldown_period = 60
 
     cpu_utilization {
