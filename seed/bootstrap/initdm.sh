9baeb50841202ebc960e9db64ab10394e8918820
dmalicia <dmalicia@gmail.com>
2020-09-24 17:01:21 -0400

    Removing old unused files from first setup

--- seed/bootstrap/initdm.sh
+++ seed/bootstrap/initdm.sh
@@ -10,7 +10,7 @@ sudo apt-get update
 sudo apt-get -yq install docker-ce docker-ce-cli containerd.io;
 git clone https://github.com/dmalicia/dm-terraform.git /usr/local/share/dm-terraform/
 sleep 10;
-docker run --name atlantis -d -p 4141:4141 -v /usr/local/share/dm-terraform:/usr/local/share/dm-terraform runatlantis/atlantis server --gh-user=dmalicia --gh-token=c4d5e72d0ce72e6dc95a966f5cdee528f8115843 --repo-allowlist github.com/dmalicia/dm-terraform --repo-config=/usr/local/share/dm-terraform/seed/bootstrap/repos.yaml
+docker run --name atlantis -d -p 4141:4141 -v /usr/local/share/dm-terraform:/usr/local/share/dm-terraform runatlantis/atlantis server --gh-user=dmalicia --gh-token=$GH_token --repo-allowlist github.com/dmalicia/dm-terraform --repo-config=/usr/local/share/dm-terraform/seed/bootstrap/repos.yaml
 docker restart atlantis
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip /
 unzip /ngrok-stable-linux-amd64.zip 
